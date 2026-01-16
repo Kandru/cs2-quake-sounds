5a166fb16eede5fc1a957ffaa435fcbb36000267
derkalle4 <kalle@kandru.de>
2026-01-16 20:51:46 +0100

    bump version to 26.01.3

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace QuakeSounds
 {
     public partial class QuakeSounds
     {
-        public override string ModuleVersion => "26.01.2";
+        public override string ModuleVersion => "26.01.3";
     }
 }
